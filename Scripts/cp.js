fa82393eabf2746d4be939fbb47a7d8adfa4a0ad
Frode Petterson <frode.petterson@amendor.com>
2013-03-20 17:28:29 +0100

    Added CP lib.

---
+++ Scripts/cp.js
@@ -0,0 +1,55 @@
+var H5PEditor = H5PEditor || {};
+
+/**
+ * Create a field for the form.
+ * 
+ * @param {mixed} parent
+ * @param {Object} field
+ * @param {mixed} params
+ * @param {function} setValue
+ * @returns {H5PEditor.Text}
+ */
+H5PEditor.CoursePresentation = function (parent, field, params, setValue) {
+  this.parent = parent;
+  this.field = field;
+  this.params = params;
+  this.setValue = setValue;
+  
+  console.log(params);
+};
+
+/**
+ * Append field to wrapper.
+ * 
+ * @param {type} $wrapper
+ * @returns {undefined}
+ */
+H5PEditor.CoursePresentation.prototype.appendTo = function ($wrapper) {
+  this.$item = H5PEditor.$(this.createHtml()).appendTo($wrapper);
+  this.$editor = this.$item.children('.editor');
+  this.$errors = this.$item.children('.errors');
+};
+
+/**
+ * Create HTML for the field.
+ */
+H5PEditor.CoursePresentation.prototype.createHtml = function () {
+  return H5PEditor.createItem(this.field.widget, '<div class="editor">Presentation:)</div>');
+};
+
+/**
+ * Validate the current field.
+ */
+H5PEditor.CoursePresentation.prototype.validate = function () {
+  return true;
+};
+
+/**
+ * Remove this item.
+ */
+H5PEditor.CoursePresentation.prototype.remove = function () {
+  this.$item.remove();
+};
+
+// Tell the editor what widget we are.
+H5PEditor.widgets.coursepresentation = H5PEditor.CoursePresentation;
