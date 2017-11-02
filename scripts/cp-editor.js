825f02c4293cc0f0af0df3e0b979a7de2d0c7d1f
Andreas Nergaard <andreas.nergaard@cerpus.com>
2017-11-02 15:36:41 +0100

    HFP-1720 Added h5p-shape to library dependencies. Added special case for default styles of created shapes in CP. Added editor svg icon for shape type.

--- scripts/cp-editor.js
+++ scripts/cp-editor.js
@@ -142,6 +142,11 @@ H5PEditor.CoursePresentation.prototype.addElement = function (library, options) 
           elementParams.action.params.fitToWrapper = true;
           break;
 
+        case 'H5P.Shape':
+          elementParams.width = 10.6765;
+          elementParams.height = 21.09;
+          break;
+
         case 'H5P.DragQuestion':
           elementParams.width = 50;
           elementParams.height = 50;
