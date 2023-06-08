8c1211866580a2d13e9201378ffa25c6d379ff36
Frode Petterson <frode.andre.petterson@gmail.com>
2023-06-08 16:35:50 +0200

    JI-4596 Fix the broken active surface mode (#86)
    * JI-4596 Fix the broken active surface mode
    
    We introduced a new layout and classes for active surface mode
    but never adjusted the editor. Now we add some hacks to get
    the editor view back to its original look
    
    * JI-4596 Fix more specific CSS
    
    (Apple Mac M2 loads files in different order)
    
    * JI-4596 Fix selector broken by new wrapper from HFP-3577
    
    This fixes stretching of elements when enabling the active surface

--- scripts/cp-editor.js
+++ scripts/cp-editor.js
@@ -49,6 +49,7 @@ H5PEditor.CoursePresentation = function (parent, field, params, setValue) {
 
       // Enable adjustments
       that.cp.$container.addClass('h5p-active-surface');
+      that.cp.$wrapper.addClass('h5p-course-presentation-active-surface');
 
       // Remove navigation
       that.cp.$progressbar.remove();
@@ -104,7 +105,7 @@ H5PEditor.CoursePresentation.prototype.updateElementSizes = function (heightRati
   // Go through all slides
   for (var i = 0; i < this.params.slides.length; i++) {
     var slide = this.params.slides[i];
-    var $slideElements = $slides.eq(i).children();
+    var $slideElements = $slides.eq(i).find('.h5p-element');
 
     for (var j = 0; j < slide.elements.length; j++) {
       var element = slide.elements[j];
